dcecab9e2f177f98546833f4533585a0c1318c53
Jacob <Jacobabrooks04@gmail.com>
2023-07-12 12:10:54 +0100

    nav

---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+export {default as Home} from './HomePage';
+// export {default as CreateColor} from './CreateColor';
+// export {default as Color} from './ColorPage';
+// export {default as PickColor} from './PickColor';
